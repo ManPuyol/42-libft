d515054884a1a6019f6273ee7c6043b76c055c07
Manuel Ortega <maopmore@gmail.com>
2024-09-22 16:45:27 +0200

    Added to lower and upper

--- libft.h
+++ libft.h
@@ -22,9 +22,9 @@
 //     struct s_list	*next;
 // }	t_list;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
+int		ft_isalpha(char c);
+int		ft_isdigit(char c);
+int		ft_isalnum(char c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
