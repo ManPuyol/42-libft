b4899c4167b40ec1cccc3762386caa8a6b9c7547
Manuel Ortega <maopmore@gmail.com>
2024-09-28 14:33:28 +0200

    commenting unnecesary header

--- ft_split.c
+++ ft_split.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+/*#include <stdio.h>*/
 
 size_t	count_words(const char *s, char c)
 {
