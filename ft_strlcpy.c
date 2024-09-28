b9f5d29c58b615d9296ce109d8d143c719948810
Manuel Ortega <maopmore@gmail.com>
2024-09-28 12:53:19 +0200

    Fix allow crash

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -17,8 +17,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	src_len;
 	size_t	i;
 
-	if (!dst || !src)
-		return (0);
 	src_len = ft_strlen(src);
 	if (size == 0)
 		return (src_len);
